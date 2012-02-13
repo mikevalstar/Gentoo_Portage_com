31af9b8f6107e0d6c6ce9bc603746d551b3e4a51
Mike Valstar <mikevalstar@gmail.com>
2012-02-13 11:59:30 -0500

    Added twitter bootstrap css to project.

--- app.js
+++ app.js
@@ -3,8 +3,12 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes')
+var	express = require('express')
+	routes = require('./routes'),
+	less = require('less'),
+	path = require('path'),
+	fs = require('fs'),
+	sys = require('util');
 
 var app = module.exports = express.createServer();
 
@@ -27,8 +31,25 @@ app.configure('production', function(){
   app.use(express.errorHandler()); 
 });
 
-// Routes
+// Build (Stylesheets)
+var lessParser = new(less.Parser)({
+    paths: ['.', './style'], // Specify search paths for @import directives
+});
 
+var lessBootstrapFile = path.join(__dirname, 'style/bootstrap.less');
+var cssBootstrapFile = path.join(__dirname, 'public/css/bootstrap.css');
+
+fs.readFile(lessBootstrapFile, 'utf8', function (e, data) {
+	lessParser.parse(data, function (e, tree) {
+	    //tree.toCSS({ compress: true }); // Minify CSS output
+	    fs.writeFile(cssBootstrapFile,tree.toCSS(), function (err) {
+			if (err) throw err;
+			console.log(cssBootstrapFile + ' Written to disk!');
+		});
+	});
+});
+
+// Routes
 app.get('/', routes.index);
 
 app.listen(3030);
